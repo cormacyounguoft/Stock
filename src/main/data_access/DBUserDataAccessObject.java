dc2af133caf7ad487465bcfd6bdf85b5285bcf07
cormacyounguoft <cormac.young@mail.utoronto.ca>
2026-04-06 14:48:27 -0400

    Adding starter files

---
+++ src/main/data_access/DBUserDataAccessObject.java
@@ -0,0 +1,65 @@
+package data_access;
+
+import java.io.IOException;
+
+import entity.User;
+import entity.UserFactory;
+import use_case.change_password.ChangePasswordUserDataAccessInterface;
+import use_case.login.LoginUserDataAccessInterface;
+import use_case.logout.LogoutUserDataAccessInterface;
+import use_case.signup.SignupUserDataAccessInterface;
+
+/**
+ * The DAO for user data.
+ */
+public class DBUserDataAccessObject implements SignupUserDataAccessInterface,
+        LoginUserDataAccessInterface,
+        ChangePasswordUserDataAccessInterface,
+        LogoutUserDataAccessInterface {
+    private static final int SUCCESS_CODE = 200;
+    private static final String CONTENT_TYPE_LABEL = "Content-Type";
+    private static final String CONTENT_TYPE_JSON = "application/json";
+    private static final String STATUS_CODE_LABEL = "status_code";
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
+    private static final String MESSAGE = "message";
+    private final UserFactory userFactory;
+
+    public DBUserDataAccessObject(UserFactory userFactory) {
+        this.userFactory = userFactory;
+        // No need to do anything to reinitialize a user list! The data is the cloud that may be miles away.
+    }
+
+    @Override
+    public User get(String username) {
+        // Make an API call to get the user object.
+
+        return null;
+    }
+
+    @Override
+    public void setCurrentUsername(String name) {
+        // this isn't implemented for the lab
+    }
+
+    @Override
+    public boolean existsByName(String username) {
+
+        return false;
+    }
+
+    @Override
+    public void save(User user) {
+
+    }
+
+    @Override
+    public void changePassword(User user) {
+
+    }
+
+    @Override
+    public String getCurrentUsername() {
+        return null;
+    }
+}
