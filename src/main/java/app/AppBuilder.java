462e62cc73951bfae726d1aef5c0172c551e81f2
cormacyounguoft <cormac.young@mail.utoronto.ca>
2026-04-06 14:18:31 -0400

    Adding starter files

---
+++ src/main/java/app/AppBuilder.java
@@ -0,0 +1,185 @@
+package main.java.app;
+
+import java.awt.CardLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+import main.java.data_access.InMemoryUserDataAccessObject;
+import main.java.entity.CommonUserFactory;
+import main.java.entity.UserFactory;
+import main.java.interface_adapter.ViewManagerModel;
+import main.java.interface_adapter.change_password.ChangePasswordController;
+import main.java.interface_adapter.change_password.ChangePasswordPresenter;
+import main.java.interface_adapter.change_password.LoggedInViewModel;
+import main.java.interface_adapter.login.LoginController;
+import main.java.interface_adapter.login.LoginPresenter;
+import main.java.interface_adapter.login.LoginViewModel;
+import main.java.interface_adapter.logout.LogoutController;
+import main.java.interface_adapter.logout.LogoutPresenter;
+import main.java.interface_adapter.signup.SignupController;
+import main.java.interface_adapter.signup.SignupPresenter;
+import main.java.interface_adapter.signup.SignupViewModel;
+import main.java.use_case.change_password.ChangePasswordInputBoundary;
+import main.java.use_case.change_password.ChangePasswordInteractor;
+import main.java.use_case.change_password.ChangePasswordOutputBoundary;
+import main.java.use_case.login.LoginInputBoundary;
+import main.java.use_case.login.LoginInteractor;
+import main.java.use_case.login.LoginOutputBoundary;
+import main.java.use_case.logout.LogoutInputBoundary;
+import main.java.use_case.logout.LogoutInteractor;
+import main.java.use_case.logout.LogoutOutputBoundary;
+import main.java.use_case.signup.SignupInputBoundary;
+import main.java.use_case.signup.SignupInteractor;
+import main.java.use_case.signup.SignupOutputBoundary;
+import main.java.view.LoggedInView;
+import main.java.view.LoginView;
+import main.java.view.SignupView;
+import main.java.view.ViewManager;
+
+/**
+ * The AppBuilder class is responsible for putting together the pieces of
+ * our CA architecture; piece by piece.
+ * <p/>
+ * This is done by adding each View and then adding related Use Cases.
+ */
+// Checkstyle note: you can ignore the "Class Data Abstraction Coupling"
+//                  and the "Class Fan-Out Complexity" issues for this lab; we encourage
+//                  your team to think about ways to refactor the code to resolve these
+//                  if your team decides to work with this as your starter code
+//                  for your final project this term.
+public class AppBuilder {
+    private final JPanel cardPanel = new JPanel();
+    private final CardLayout cardLayout = new CardLayout();
+    // thought question: is the hard dependency below a problem?
+    private final UserFactory userFactory = new CommonUserFactory();
+    private final ViewManagerModel viewManagerModel = new ViewManagerModel();
+    private final ViewManager viewManager = new ViewManager(cardPanel, cardLayout, viewManagerModel);
+
+    // thought question: is the hard dependency below a problem?
+    private final InMemoryUserDataAccessObject userDataAccessObject = new InMemoryUserDataAccessObject();
+
+    private SignupView signupView;
+    private SignupViewModel signupViewModel;
+    private LoginViewModel loginViewModel;
+    private LoggedInViewModel loggedInViewModel;
+    private LoggedInView loggedInView;
+    private LoginView loginView;
+
+    public AppBuilder() {
+        cardPanel.setLayout(cardLayout);
+    }
+
+    /**
+     * Adds the Signup View to the application.
+     * @return this builder
+     */
+    public AppBuilder addSignupView() {
+        signupViewModel = new SignupViewModel();
+        signupView = new SignupView(signupViewModel);
+        cardPanel.add(signupView, signupView.getViewName());
+        return this;
+    }
+
+    /**
+     * Adds the Login View to the application.
+     * @return this builder
+     */
+    public AppBuilder addLoginView() {
+        loginViewModel = new LoginViewModel();
+        loginView = new LoginView(loginViewModel);
+        cardPanel.add(loginView, loginView.getViewName());
+        return this;
+    }
+
+    /**
+     * Adds the LoggedIn View to the application.
+     * @return this builder
+     */
+    public AppBuilder addLoggedInView() {
+        loggedInViewModel = new LoggedInViewModel();
+        loggedInView = new LoggedInView(loggedInViewModel);
+        cardPanel.add(loggedInView, loggedInView.getViewName());
+        return this;
+    }
+
+    /**
+     * Adds the Signup Use Case to the application.
+     * @return this builder
+     */
+    public AppBuilder addSignupUseCase() {
+        final SignupOutputBoundary signupOutputBoundary = new SignupPresenter(viewManagerModel,
+                signupViewModel, loginViewModel);
+        final SignupInputBoundary userSignupInteractor = new SignupInteractor(
+                userDataAccessObject, signupOutputBoundary, userFactory);
+
+        final SignupController controller = new SignupController(userSignupInteractor);
+        signupView.setSignupController(controller);
+        return this;
+    }
+
+    /**
+     * Adds the Login Use Case to the application.
+     * @return this builder
+     */
+    public AppBuilder addLoginUseCase() {
+        final LoginOutputBoundary loginOutputBoundary = new LoginPresenter(viewManagerModel,
+                loggedInViewModel, loginViewModel);
+        final LoginInputBoundary loginInteractor = new LoginInteractor(
+                userDataAccessObject, loginOutputBoundary);
+
+        final LoginController loginController = new LoginController(loginInteractor);
+        loginView.setLoginController(loginController);
+        return this;
+    }
+
+    /**
+     * Adds the Change Password Use Case to the application.
+     * @return this builder
+     */
+    public AppBuilder addChangePasswordUseCase() {
+        final ChangePasswordOutputBoundary changePasswordOutputBoundary =
+                new ChangePasswordPresenter(loggedInViewModel);
+
+        final ChangePasswordInputBoundary changePasswordInteractor =
+                new ChangePasswordInteractor(userDataAccessObject, changePasswordOutputBoundary, userFactory);
+
+        final ChangePasswordController changePasswordController =
+                new ChangePasswordController(changePasswordInteractor);
+        loggedInView.setChangePasswordController(changePasswordController);
+        return this;
+    }
+
+    /**
+     * Adds the Logout Use Case to the application.
+     * @return this builder
+     */
+    public AppBuilder addLogoutUseCase() {
+        final LogoutOutputBoundary logoutOutputBoundary = new LogoutPresenter(viewManagerModel,
+                loggedInViewModel, loginViewModel);
+
+        final LogoutInputBoundary logoutInteractor =
+                new LogoutInteractor(userDataAccessObject, logoutOutputBoundary);
+
+        final LogoutController logoutController = new LogoutController(logoutInteractor);
+        loggedInView.setLogoutController(logoutController);
+        return this;
+    }
+
+    /**
+     * Creates the JFrame for the application and initially sets the SignupView to be displayed.
+     * @return the application
+     */
+    public JFrame build() {
+        final JFrame application = new JFrame("Login Example");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        application.add(cardPanel);
+
+        viewManagerModel.setState(signupView.getViewName());
+        viewManagerModel.firePropertyChanged();
+
+        return application;
+    }
+}
---
+++ src/main/java/interface_adapter/login/LoginViewModel.java
@@ -0,0 +1,15 @@
+package main.java.interface_adapter.login;
+
+import main.java.interface_adapter.ViewModel;
+
+/**
+ * The View Model for the Login View.
+ */
+public class LoginViewModel extends ViewModel<LoginState> {
+
+    public LoginViewModel() {
+        super("log in");
+        setState(new LoginState());
+    }
+
+}
